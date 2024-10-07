b163b387e3786dfc204df7a9e39fe1b3cd27b71c
djimmydz <165382433+djimmydz@users.noreply.github.com>
2024-10-07 16:10:01 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -28,13 +28,13 @@ const app = {
             const scrollY = window.scrollY;
             
             if (scrollY > 1900) {
-                image.style.backgroundImage = "url('../images/header_bg3.avif')";
+                image.style.backgroundImage = "url('../images/header_bg3.jpg')";
             } else if (scrollY > 1100) {
-                image.style.backgroundImage = "url('../images/header-bg1.avif')";
+                image.style.backgroundImage = "url('../images/header-bg1.jpg')";
             } else if (scrollY > 500) {
-                image.style.backgroundImage = "url('../images/header-bg2.avif')";
+                image.style.backgroundImage = "url('../images/header-bg2.jpg')";
             } else {
-                image.style.backgroundImage = "url('../images/header-bg.avif')";
+                image.style.backgroundImage = "url('../images/header-bg.jpg')";
             }
 
             image.style.backgroundSize = 'cover';
